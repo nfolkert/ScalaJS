2963d7ca64c2e560f25a8d1abd64ed2dc3ff795a
nfolkert <nfolkert@foursquare.com>
2011-04-08 11:02:55 -0400

    Enable sbt testing

--- project/build/ScalaJSProject.scala
+++ project/build/ScalaJSProject.scala
@@ -5,5 +5,9 @@ class ScalaJSProject(info: ProjectInfo) extends DefaultProject(info) {
 
   val specs = "org.scala-tools.testing" %% "specs" % "1.6.5" % "test" withSources()
   val rhino = "rhino" % "js" % "1.7R2" % "compile" withSources()
-  val junit = "junit" % "junit" % "4.8.1" % "test" withSources()
+  val junit = "junit" % "junit" % "4.8.2" % "test" withSources()
+
+  val bryanjswift = "Bryan J Swift Repository" at "http://repos.bryanjswift.com/maven2/"
+  val junitInterface = "com.novocode" % "junit-interface" % "0.6" % "test"
+  override def testFrameworks = super.testFrameworks ++ List(new TestFramework("com.novocode.junit.JUnitFrameworkNoMarker"))
 }
