aaa708d81288ac76305cf3c839b2f2d311157e33
nfolkert <nfolkert@foursquare.com>
2011-04-30 18:27:38 -0400

    Add a few more functions

--- src/main/scala/org/scalajs/Conversions.scala
+++ src/main/scala/org/scalajs/Conversions.scala
@@ -4,24 +4,37 @@ import reflect.ClassManifest
 import org.mozilla.javascript.{Scriptable, Context, Callable}
 
 class Conversions(ctx: Context, scope: Scriptable) {
-  def unwrap[T](v: AnyRef)(implicit mf: Manifest[T]): T = {
 
-    val argClasses = mf.typeArguments.map(_.erasure)
-    def call(c: Callable, args: Array[AnyRef]) = {
+  private def unwrapBasic(v: AnyRef, mf: Manifest[_]): AnyRef = {
+    Context.jsToJava(v, mf.erasure)
+  }
+
+  def unwrap[T](v: AnyRef)(implicit mf: Manifest[T]): T = {
+    def call(c: Callable, args: Array[AnyRef]): AnyRef = {
       val ret = c.call(ctx, scope, null, args)
-      Context.jsToJava(ret, argClasses.last)
+      unwrapBasic(ret, mf.typeArguments.last)
     }
 
     v match {
-      case c: Callable => {
+      case fn: Callable => {
         val cf0 = classOf[Function0[_]]
         val cf1 = classOf[Function1[_,_]]
         val cf2 = classOf[Function2[_,_,_]]
+        val cf3 = classOf[Function3[_,_,_,_]]
+        val cf4 = classOf[Function4[_,_,_,_,_]]
+        val cf5 = classOf[Function5[_,_,_,_,_,_]]
+        val cf6 = classOf[Function6[_,_,_,_,_,_,_]]
 
+        type A = AnyRef
         mf.erasure match {
-          case v if v == cf0 => (() => call(c, new Array(0))).asInstanceOf[T]
-          case v if v == cf1 => ((a:AnyRef) => call(c, Array[AnyRef](a))).asInstanceOf[T]
-          case v if v == cf2 => ((a:AnyRef,b:AnyRef) => call(c, Array[AnyRef](a, b))).asInstanceOf[T]
+          case v if v == cf0 => (() => call(fn, new Array(0))).asInstanceOf[T]
+          case v if v == cf1 => ((a:A) => call(fn, Array[A](a))).asInstanceOf[T]
+          case v if v == cf2 => ((a:A,b:A) => call(fn, Array[A](a,b))).asInstanceOf[T]
+          case v if v == cf3 => ((a:A,b:A,c:A) => call(fn, Array[A](a,b,c))).asInstanceOf[T]
+          case v if v == cf4 => ((a:A,b:A,c:A,d:A) => call(fn, Array[A](a,b,c,d))).asInstanceOf[T]
+          case v if v == cf5 => ((a:A,b:A,c:A,d:A,e:A) => call(fn, Array[A](a,b,c,d,e))).asInstanceOf[T]
+          case v if v == cf6 => ((a:A,b:A,c:A,d:A,e:A,f:A) => call(fn, Array[A](a,b,c,d,e,f))).asInstanceOf[T]
+
           case _ => throw new UnsupportedOperationException()
         }
       }
--- src/test/scala/org/scalajs/scalajs/CallbacksSpec.scala
+++ src/test/scala/org/scalajs/scalajs/CallbacksSpec.scala
@@ -23,10 +23,18 @@ object CallbacksSpec extends Specification("Callbacks Specification") {
       val cbk0  = conversion.unwrap[()=>Int](eval("var x = function() {return 0}; x"))
       val cbk1  = conversion.unwrap[Int=>Int](eval("var x = function(a) {return a + 1}; x"))
       val cbk2 = conversion.unwrap[(Int,Int)=>Int](eval("var x = function(a, b) {return a + b}; x"))
+      val cbk3 = conversion.unwrap[(Int,Int,Int)=>Int](eval("var x = function(a, b, c) {return a + b + c}; x"))
+      val cbk4 = conversion.unwrap[(Int,Int,Int,Int)=>Int](eval("var x = function(a, b, c, d) {return a + b + c + d}; x"))
+      val cbk5 = conversion.unwrap[(Int,Int,Int,Int,Int)=>Int](eval("var x = function(a, b, c, d, e) {return a + b + c + d + e}; x"))
+      val cbk6 = conversion.unwrap[(Int,Int,Int,Int,Int,Int)=>Int](eval("var x = function(a, b, c, d, e, f) {return a + b + c + d + e + f}; x"))
 
       List(1, 2, 3).map((a) => cbk0()) must_== List(0, 0, 0)
       List(1, 2, 3).map(cbk1) must_== List(2, 3, 4)
       List((1,2), (2,3), (3,4)).map(cbk2.tupled) must_== List(3, 5, 7)
+      List((1,2,3), (2,3,4), (3,4,5)).map(cbk3.tupled) must_== List(6, 9, 12)
+      List((1,2,3,4), (2,3,4,5), (3,4,5,6)).map(cbk4.tupled) must_== List(10, 14, 18)
+      List((1,2,3,4,5), (2,3,4,5,6), (3,4,5,6,7)).map(cbk5.tupled) must_== List(15, 20, 25)
+      List((1,2,3,4,5,6), (2,3,4,5,6,7), (3,4,5,6,7,8)).map(cbk6.tupled) must_== List(21, 27, 33)
     }
   }
 }
